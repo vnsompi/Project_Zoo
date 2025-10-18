4bb2edf8478ca881962524fbd4f513238edaae8b
vainqueur <vnsompi@gmail.com>
2025-10-18 14:17:41 +0100

    added ticket model to the admin.py

--- zooApi/admin.py
+++ zooApi/admin.py
@@ -46,4 +46,5 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Reservation)
 admin.site.register(models.Event)
+admin.site.register(models.Ticket)
 
